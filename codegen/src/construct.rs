ee338e2129675935f49ef55b74ca0b3431cca971
Robert Oleynik <robert.oleynik@proton.me>
2023-02-18 11:15:19 +0100

    feat: parse construct

--- codegen/src/construct.rs
+++ codegen/src/construct.rs
@@ -1,6 +1,6 @@
 use syn::parse::{Parse, ParseStream};
 use syn::punctuated::Punctuated;
-use syn::Token;
+use syn::{Attribute, Ident, LitInt, Token, Type};
 
 pub struct Construct {
     pub pub_token: Token![pub],
@@ -10,11 +10,33 @@ pub struct Construct {
 }
 
 pub struct Field {
+    pub attributes: Vec<Attribute>,
+    pub auto: Option<Token![auto]>,
     pub name: syn::Ident,
+    pub ty: FieldType,
     pub colon_token: Token![:],
 }
 
-pub enum FieldType {}
+pub enum FieldType {
+    Object {
+        fields: Punctuated<Field, Token![,]>,
+    },
+    Map {
+        key_ty: Type,
+        nested: Box<FieldType>,
+    },
+    List {
+        min: Option<LitInt>,
+        max: Option<LitInt>,
+        nested: Box<FieldType>,
+    },
+    Set {
+        nested: Box<FieldType>,
+    },
+    Type {
+        ty: syn::Type,
+    },
+}
 
 impl Parse for Construct {
     fn parse(input: ParseStream) -> syn::Result<Self> {
@@ -32,14 +54,59 @@ impl Parse for Construct {
 
 impl Parse for Field {
     fn parse(input: ParseStream) -> syn::Result<Self> {
+        let attributes = input.call(Attribute::parse_outer)?;
+        let auto = input.parse()?;
         let name = input.parse()?;
         let colon_token = input.parse()?;
-        Ok(Self { name, colon_token })
+        let ty = input.parse()?;
+        Ok(Self {
+            attributes,
+            auto,
+            name,
+            colon_token,
+            ty,
+        })
     }
 }
 
 impl Parse for FieldType {
     fn parse(input: ParseStream) -> syn::Result<Self> {
-        todo!()
+        let lookahead = input.lookahead1();
+        if lookahead.peek(syn::token::Brace) {
+            let content;
+            syn::braced!(content in input);
+            let fields = content.parse_terminated(Field::parse)?;
+            Ok(FieldType::Object { fields })
+        } else if lookahead.peek(syn::token::Bracket) {
+            let content;
+            syn::bracketed!(content in input);
+            if content.is_empty() {
+                input.parse::<Token![=>]>()?;
+                let nested = input.parse()?;
+                return Ok(FieldType::Set { nested });
+            }
+
+            let lookahead = content.lookahead1();
+            if lookahead.peek(Token![..]) || lookahead.peek(syn::LitInt) {
+                let min = content.parse()?;
+                content.parse::<Token![..]>()?;
+                let max = content.parse()?;
+                input.parse::<Token![=>]>()?;
+                let nested = input.parse()?;
+                return Ok(FieldType::List { min, max, nested });
+            } else if lookahead.peek(Token![::]) || lookahead.peek(Ident) {
+                let key_ty = content.parse()?;
+                input.parse::<Token![=>]>()?;
+                let nested = input.parse()?;
+                return Ok(FieldType::Map { key_ty, nested });
+            } else {
+                Err(lookahead.error())
+            }
+        } else if lookahead.peek(Token![::]) || lookahead.peek(Ident) {
+            let ty = input.parse()?;
+            Ok(FieldType::Type { ty })
+        } else {
+            Err(lookahead.error())
+        }
     }
 }
